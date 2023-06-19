de4ffd5371930df31d393225c331fe10b875cc11
Ondrej Sika <ondrej@ondrejsika.com>
2023-06-19 15:33:01 +0200

    test: Fix test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	msg := Hello("Dela")
-	if msg != "Hello Dela" {
-		t.Fatalf(`Hello("Dela") = %s; want "Hello Dela"`, msg)
+	if msg != "Hello Dela!" {
+		t.Fatalf(`Hello("Dela") = %s; want "Hello Dela!"`, msg)
 	}
 }
